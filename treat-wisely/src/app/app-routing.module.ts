d8f8faca5b9966b8587cb5b287441a5ced47568d
Elliot <56415058+elliotrpmorris@users.noreply.github.com>
2020-05-10 21:37:13 +0100

    add gallery

--- treat-wisely/src/app/app-routing.module.ts
+++ treat-wisely/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { ContactComponent } from "./contact/contact.component";
 import { AboutComponent } from "./about/about.component";
+import { GalleryComponent } from "./gallery/gallery.component";
 
 const routes: Routes = [
   { path: "about", component: AboutComponent },
   { path: "contact", component: ContactComponent },
+  { path: "gallery", component: GalleryComponent },
   { path: "", redirectTo: "/", pathMatch: "full" },
   { path: "**", component: HomeComponent },
 ];
